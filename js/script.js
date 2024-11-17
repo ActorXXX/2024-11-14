7c94e8886452cc7c0401c30781a8d7bd5cd1aa9b
ActorXXX <jose_rios@outlook.com>
2024-11-17 02:02:43 -0500

    al fin terminado

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ async function mostrarCafes() {
     cafecitos = cafes
     
     cafecitos.forEach(cafe => {
+        console.log(cafe)
         crearCafe(cafe)
     })
 }
@@ -27,30 +28,48 @@ async function mostrarCafes() {
 function crearCafe(cafe) {
     //obtenemos el DIV donde vamos a mostrar todos los cafecitos
     const cafecitos = document.getElementById('cafecitos')
-    cafecitos.style.display = 'flex'
-    cafecitos.style.flexWrap = 'wrap'
-    cafecitos.style.gap = '1em'
 
-    //empezamos con la construccion del cafecito
+    if(!cafecitos) return
+
+//EMPEZAMOS CON LA CONSTRUCCION DE LA TARJETA
     const div = document.createElement('div')
     div.classList.add('cafe')
     
 
+//CARDHEADER - cabecera de la tarjeta
 
     const cardHeader = document.createElement('div')
+    cardHeader.classList.add('cardHeader')
 
+    //image
     const image = document.createElement('img')
+    image.classList.add('image')
     image.src = cafe.image
     image.style.borderRadius = '10px'
 
     cardHeader.appendChild(image)
+    
+     //popular
+     const popular = document.createElement('div')
+     popular.classList.add('popular')
+ 
+     if(cafe.popular == true
+         ? (popular.textContent = 'Popular')
+         : (popular.style.visibility = 'hidden')
+     )
+ 
+     cardHeader.appendChild(popular)
+ 
+ 
+    
+    
 
-//CARDFOOTER
+//CARDFOOTER - informacion de la tarjeta
 
     const cardFooter = document.createElement('div')
     cardFooter.classList.add('cardFooter')
 
-    //card description
+    //card Description
     const cardDescription = document.createElement('div')
     cardDescription.classList.add('cardDescription')
     
@@ -69,39 +88,57 @@ function crearCafe(cafe) {
     cardDescription.appendChild(price)
 
 
-    //rating
-    const rating = document.createElement('div')
-    rating.classList.add('rating')
-    rating.style.marginTop = '.6em'
+    //card Rating
+    const cardRating = document.createElement('div')
+    cardRating.classList.add('cardRating')
     
     //star
     const star = document.createElement('img')
     
     //cabiando la imagen de la estrellita
-    if(cafe.rating == null || cafe.votes == 0 ?
+    if(cafe.rating == null ?
         (star.src = star_blank) : (star.src = star_fill))
 
-    rating.appendChild(star)
+    cardRating.appendChild(star)
+
+    //rating
+    const rating = document.createElement('div')
+    rating.classList.add('rating')
+ 
+    if (cafe.rating == null
+        ? (rating.textContent = '')
+        : (rating.textContent = cafe.rating))
+
+    cardRating.appendChild(rating)
 
     //votes
     const votes = document.createElement('div')
+    votes.classList.add('votes')
 
     //validando si hay votos o no
-    if (cafe.rating == null || cafe.votes == 0
-        ? (votes.textContent = 'No rating')
-        : (votes.textContent = cafe.rating))
+    if (cafe.votes == 0
+        ? (votes.textContent = 'No votes')
+        : (votes.textContent = '(' + cafe.votes + ' votes)'))
+    
 
-    rating.appendChild(votes)
+    cardRating.appendChild(votes)
 
+    //available
+    const available = document.createElement('div')
+    available.classList.add('available')
 
+    if (cafe.available == false 
+        ? (available.textContent = 'Sold out')
+        : (available.textContent = '')
+    )
 
-
-
+    cardRating.appendChild(available)
 
 
 
     cardFooter.appendChild(cardDescription)
-    cardFooter.appendChild(rating)
+    cardFooter.appendChild(cardRating)
+
 
 
     //agregamos los hijos al contenedor padre
